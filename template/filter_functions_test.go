f5b5f68e13d2f1a4fba7987ef1a30a775142dac1
Rene Kaufmann <kaufmann.r@gmail.com>
2016-06-29 19:45:59 +0200

    more tests

--- template/filter_functions_test.go
+++ template/filter_functions_test.go
@@ -4,57 +4,58 @@ import (
 	"testing"
 
 	"github.com/flosch/pongo2"
+	"github.com/kelseyhightower/memkv"
+	. "gopkg.in/check.v1"
 )
 
-func TestFilterBase64(t *testing.T) {
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type FilterSuite struct{}
+
+var _ = Suite(&FilterSuite{})
+
+func (s *FilterSuite) TestFilterBase64(t *C) {
 	in := pongo2.AsValue("foo")
 	res, err := filterBase64(in, nil)
 	if err != nil {
 		t.Error(err.ErrorMsg)
 	}
 
-	if res.String() != "Zm9v" {
-		t.Errorf("Expected filterBase64 to be %v, got %v", "Zm9v", res.String())
-	}
+	t.Check(res.String(), Equals, "Zm9v")
 }
 
-func TestFilterBase(t *testing.T) {
+func (s *FilterSuite) TestFilterBase(t *C) {
 	in := pongo2.AsValue("/etc/foo/bar")
 	res, err := filterBase(in, nil)
 	if err != nil {
 		t.Error(err.ErrorMsg)
 	}
 
-	if res.String() != "bar" {
-		t.Errorf("Expected filterBase to be %v, got %v", "bar", res.String())
-	}
+	t.Check(res.String(), Equals, "bar")
 }
 
-func TestFilterDir(t *testing.T) {
+func (s *FilterSuite) TestFilterDir(t *C) {
 	in := pongo2.AsValue("/etc/foo/bar")
 	res, err := filterDir(in, nil)
 	if err != nil {
 		t.Error(err.ErrorMsg)
 	}
 
-	if res.String() != "/etc/foo" {
-		t.Errorf("Expected filterDir to be %v, got %v", "/etc/foo", res.String())
-	}
+	t.Check(res.String(), Equals, "/etc/foo")
 }
 
-func TestFilterSplit(t *testing.T) {
+func (s *FilterSuite) TestFilterSplit(t *C) {
 	in := pongo2.AsValue("foo/bar")
 	res, err := filterSplit(in, pongo2.AsValue("/"))
 	if err != nil {
 		t.Error(err.ErrorMsg)
 	}
 
-	if res.Interface().([]string)[0] != "foo" || res.Interface().([]string)[1] != "bar" {
-		t.Errorf("Expected filterSplit to be %v, got %v", []string{"foo", "bar"}, res.Interface())
-	}
+	t.Check(res.Interface().([]string), DeepEquals, []string{"foo", "bar"})
 }
 
-func TestFilterToPrettyJson(t *testing.T) {
+func (s *FilterSuite) TestFilterToPrettyJson(t *C) {
 	expected := `{
     "test": "bla",
     "test2": 1,
@@ -70,12 +71,10 @@ func TestFilterToPrettyJson(t *testing.T) {
 		t.Error(err.ErrorMsg)
 	}
 
-	if res.String() != expected {
-		t.Errorf("Expected filterToPrettyJson to be %v, got %v", expected, res.String())
-	}
+	t.Check(res.String(), Equals, expected)
 }
 
-func TestFilterToJson(t *testing.T) {
+func (s *FilterSuite) TestFilterToJson(t *C) {
 	expected := `{"test":"bla","test2":1,"test3":2.5}`
 	in := pongo2.AsValue(map[string]interface{}{
 		"test":  "bla",
@@ -86,7 +85,73 @@ func TestFilterToJson(t *testing.T) {
 	if err != nil {
 		t.Error(err.ErrorMsg)
 	}
-	if res.String() != expected {
-		t.Errorf("Expected filterToJson to be %v, got %v", expected, res.String())
+
+	t.Check(res.String(), Equals, expected)
+}
+
+func (s *FilterSuite) TestFilterUnmarshalJSONObject(t *C) {
+	in := pongo2.AsValue(`{"test":"bla","test2":"1","test3":"2.5"}`)
+	expected := map[string]interface{}{
+		"test":  "bla",
+		"test2": "1",
+		"test3": "2.5",
 	}
+	res, err := filterUnmarshalJSONObject(in, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m1 := res.Interface().(map[string]interface{})
+	t.Check(m1, DeepEquals, expected)
+}
+
+func (s *FilterSuite) TestFilterUnmarshalJSONArray(t *C) {
+	in := pongo2.AsValue(`["a", "b", "c"]`)
+	expected := []interface{}{"a", "b", "c"}
+	res, err := filterUnmarshalJSONArray(in, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m1 := res.Interface().([]interface{})
+	t.Check(m1, DeepEquals, expected)
+}
+
+func (s *FilterSuite) TestFilterSortByLengthString(t *C) {
+	in := pongo2.AsValue([]string{"123", "foobar", "1234"})
+	expected := []string{"123", "1234", "foobar"}
+	expectedRev := []string{"foobar", "1234", "123"}
+	res, err := filterSortByLength(in, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m1 := res.Interface().([]string)
+	t.Check(m1, DeepEquals, expected)
+
+	rev, err := filterReverse(res, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m2 := rev.Interface().([]string)
+	t.Check(m2, DeepEquals, expectedRev)
+}
+
+func (s *FilterSuite) TestFilterSortByLengthKVPair(t *C) {
+	a := memkv.KVPair{Key: "123", Value: "Test"}
+	b := memkv.KVPair{Key: "1234", Value: "Test"}
+	c := memkv.KVPair{Key: "foobar", Value: "Test"}
+	in := pongo2.AsValue([]memkv.KVPair{a, c, b})
+	expected := []memkv.KVPair{a, b, c}
+	expectedRev := []memkv.KVPair{c, b, a}
+	res, err := filterSortByLength(in, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m1 := res.Interface().([]memkv.KVPair)
+	t.Check(m1, DeepEquals, expected)
+
+	rev, err := filterReverse(res, nil)
+	if err != nil {
+		t.Error(err.ErrorMsg)
+	}
+	m2 := rev.Interface().([]memkv.KVPair)
+	t.Check(m2, DeepEquals, expectedRev)
 }
