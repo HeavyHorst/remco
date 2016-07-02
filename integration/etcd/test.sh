a45a37719526d331fce6e94ff9f479eeb3e933d9
Rene Kaufmann <kaufmann.r@gmail.com>
2016-07-03 00:12:44 +0200

    alias was not working, so removed it

--- integration/etcd/test.sh
+++ integration/etcd/test.sh
@@ -16,14 +16,12 @@ remco poll --onetime  etcd \
 
 cmp /tmp/remco-basic-test.conf ./integration/config/test.config || cat /tmp/remco-basic-test.conf
 
-alias etcdctl3="docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl"
-
-etcdctl3 --endpoints=127.0.0.1:2379 put /database/host 127.0.0.1
-etcdctl3 --endpoints=127.0.0.1:2379 put /database/password p@sSw0rd
-etcdctl3 --endpoints=127.0.0.1:2379 put /database/port 3306
-etcdctl3 --endpoints=127.0.0.1:2379 put /database/username remco
-etcdctl3 --endpoints=127.0.0.1:2379 put /upstream/app1 10.0.1.10:8080
-etcdctl3 --endpoints=127.0.0.1:2379 put /upstream/app2 10.0.1.11:8080
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/host 127.0.0.1
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/password p@sSw0rd
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/port 3306
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/username remco
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /upstream/app1 10.0.1.10:8080
+docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /upstream/app2 10.0.1.11:8080
 
 remco poll --onetime  etcd \
     --log-level=debug \
