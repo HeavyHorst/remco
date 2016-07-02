80f5178784274643f3f667920f18aae12f8fcb61
Rene Kaufmann <kaufmann.r@gmail.com>
2016-07-02 22:04:55 +0200

    compare rendered template to a default one - should be the same

--- integration/consul/test.sh
+++ integration/consul/test.sh
@@ -5,7 +5,7 @@
 curl -X PUT http://127.0.0.1:8500/v1/kv/database/host -d '127.0.0.1'
 curl -X PUT http://127.0.0.1:8500/v1/kv/database/password -d 'p@sSw0rd'
 curl -X PUT http://127.0.0.1:8500/v1/kv/database/port -d '3306'
-curl -X PUT http://127.0.0.1:8500/v1/kv/database/username -d 'confd'
+curl -X PUT http://127.0.0.1:8500/v1/kv/database/username -d 'remco'
 curl -X PUT http://127.0.0.1:8500/v1/kv/upstream/app1 -d '10.0.1.10:8080'
 curl -X PUT http://127.0.0.1:8500/v1/kv/upstream/app2 -d '10.0.1.11:8080'
 
@@ -16,4 +16,4 @@ remco poll --onetime  consul \
     --keys=/database/host, /database/password, /database/port, /database/username \
     --nodes=127.0.0.1:8500
 
-cat /tmp/remco-basic-test.conf
+cmp /tmp/remco-basic-test.conf ./integration/config/test.config || cat /tmp/remco-basic-test.conf
