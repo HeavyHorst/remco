3c32206844bf13ef2bd91894d485ec8b8d288117
Rene Kaufmann <kaufmann.r@gmail.com>
2016-07-05 23:31:26 +0200

    added decrypt filter function so that we can use remco together with crypt

--- template/filter_functions.go
+++ template/filter_functions.go
@@ -3,9 +3,12 @@ package template
 import (
 	"encoding/base64"
 	"encoding/json"
+	"os"
 	"path"
 	"sort"
 	"strings"
+
+	"golang.org/x/crypto/openpgp"
 
 	"github.com/HeavyHorst/memkv"
 	"github.com/cloudflare/cfssl/log"
@@ -23,6 +26,7 @@ func init() {
 	pongo2.RegisterFilter("dir", filterDir)
 	pongo2.RegisterFilter("base", filterBase)
 	pongo2.RegisterFilter("base64", filterBase64)
+	pongo2.RegisterFilter("decrypt", filterDecrypt)
 }
 
 func filterBase64(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
@@ -57,8 +61,7 @@ func filterSplit(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.
 func filterToPrettyJson(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	b, err := json.MarshalIndent(in.Interface(), "", "    ")
 	if err != nil {
-		log.Warning(err)
-		return in, nil
+		return nil, &pongo2.Error{ErrorMsg: err.Error()}
 	}
 	return pongo2.AsSafeValue(string(b)), nil
 }
@@ -66,8 +69,7 @@ func filterToPrettyJson(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *
 func filterToJson(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	b, err := json.Marshal(in.Interface())
 	if err != nil {
-		log.Warning(err)
-		return in, nil
+		return nil, &pongo2.Error{ErrorMsg: err.Error()}
 	}
 	return pongo2.AsSafeValue(string(b)), nil
 }
@@ -138,4 +140,62 @@ func filterReverse(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo
 	}
 
 	return pongo2.AsValue(values), nil
+}
+
+func filterDecrypt(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	if !param.IsString() {
+		return in, nil
+	}
+
+	secretKeyring, err := os.Open(param.String())
+	if err != nil {
+		return nil, &pongo2.Error{ErrorMsg: err.Error()}
+	}
+
+	defer secretKeyring.Close()
+	entityList, err := openpgp.ReadArmoredKeyRing(secretKeyring)
+	if err != nil {
+		return nil, &pongo2.Error{ErrorMsg: err.Error()}
+	}
+
+	input := in.Interface()
+	switch input.(type) {
+	case string:
+		i := input.(string)
+		data, err := decrypt(i, entityList)
+		if err != nil {
+			return nil, &pongo2.Error{ErrorMsg: err.Error()}
+		}
+		return pongo2.AsValue(data), nil
+	case memkv.KVPairs:
+		i := input.(memkv.KVPairs)
+		var new []memkv.KVPair
+		for _, v := range i {
+			dvalue, err := decrypt(v.Value, entityList)
+			if err != nil {
+				log.Warningf("Couldn't decrypt `%s` - %s", v.Value, err)
+			}
+			new = append(new, memkv.KVPair{Key: v.Key, Value: dvalue})
+		}
+		return pongo2.AsValue(memkv.KVPairs(new)), nil
+	case memkv.KVPair:
+		i := input.(memkv.KVPair)
+		dvalue, err := decrypt(i.Value, entityList)
+		if err != nil {
+			log.Warningf("Couldn't decrypt `%s` - %s", i.Value, err)
+		}
+		return pongo2.AsValue(memkv.KVPair{Key: i.Key, Value: dvalue}), nil
+	case []string:
+		i := input.([]string)
+		var new []string
+		for _, v := range i {
+			dvalue, err := decrypt(v, entityList)
+			if err != nil {
+				log.Warningf("Couldn't decrypt `%s` - %s", v, err)
+			}
+			new = append(new, dvalue)
+		}
+		return pongo2.AsValue(new), nil
+	}
+	return in, nil
 }
--- template/util.go
+++ template/util.go
@@ -1,6 +1,14 @@
 package template
 
-import "path"
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"io/ioutil"
+	"path"
+
+	"golang.org/x/crypto/openpgp"
+)
 
 func appendPrefix(prefix string, keys []string) []string {
 	s := make([]string, len(keys))
@@ -8,4 +16,23 @@ func appendPrefix(prefix string, keys []string) []string {
 		s[i] = path.Join(prefix, k)
 	}
 	return s
+}
+
+func decrypt(data string, entityList openpgp.EntityList) (string, error) {
+	// Taken from crypt and adapted
+	decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewBufferString(data))
+	md, err := openpgp.ReadMessage(decoder, entityList, nil, nil)
+	if err != nil {
+		return data, err
+	}
+	gzReader, err := gzip.NewReader(md.UnverifiedBody)
+	if err != nil {
+		return data, err
+	}
+	defer gzReader.Close()
+	bytes, err := ioutil.ReadAll(gzReader)
+	if err != nil {
+		return data, err
+	}
+	return string(bytes), nil
 }
