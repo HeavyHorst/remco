49ca4231472a31a762493a5417813f43dabba15e
Rene Kaufmann <kaufmann.r@gmail.com>
2016-07-16 23:20:41 +0200

    user container based travis infrastructure

--- integration/etcd/test.sh
+++ integration/etcd/test.sh
@@ -7,12 +7,14 @@ curl -L -X PUT http://127.0.0.1:2379/v2/keys/database/username -d value=remco
 curl -L -X PUT http://127.0.0.1:2379/v2/keys/upstream/app1 -d value=10.0.1.10:8080
 curl -L -X PUT http://127.0.0.1:2379/v2/keys/upstream/app2 -d value=10.0.1.11:8080
 
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/host 127.0.0.1
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/password p@sSw0rd
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/port 3306
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /database/username remco
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /upstream/app1 10.0.1.10:8080
-docker run -ti --rm -e ETCDCTL_API=3 --net=host quay.io/coreos/etcd:v3.0.1 etcdctl --endpoints=127.0.0.1:2379 put /upstream/app2 10.0.1.11:8080
+export ETCDCTL_API=3
+
+etcdctl --endpoints=127.0.0.1:2379 put /database/host 127.0.0.1
+etcdctl --endpoints=127.0.0.1:2379 put /database/password p@sSw0rd
+etcdctl --endpoints=127.0.0.1:2379 put /database/port 3306
+etcdctl --endpoints=127.0.0.1:2379 put /database/username remco
+etcdctl --endpoints=127.0.0.1:2379 put /upstream/app1 10.0.1.10:8080
+etcdctl --endpoints=127.0.0.1:2379 put /upstream/app2 10.0.1.11:8080
 
 remco config file -c integration/etcd/etcd.toml
 
--- integration/vault/test.sh
+++ integration/vault/test.sh
@@ -11,7 +11,9 @@ vault write database/host value=127.0.0.1
 vault write database/port value=3306
 vault write database/username value=remco
 vault write database/password value=p@sSw0rd
-vault write upstream app1=10.0.1.10:8080 app2=10.0.1.11:8080
+vault write upstream/app1 value=10.0.1.10:8080
+vault write upstream/app2 value=10.0.1.11:8080
+
 
 remco config file -c integration/vault/vault.toml
 cmp /tmp/remco-basic-test.conf ./integration/config/test.config || cat /tmp/remco-basic-test.conf
