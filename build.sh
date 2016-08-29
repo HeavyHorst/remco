6bff6a8155bf0c15cd08f3bab718e7a5a3682144
Rene Kaufmann <kaufmann.r@gmail.com>
2016-08-29 20:17:17 +0200

    modified build script to produce static binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -ldflags "-X github.com/HeavyHorst/remco/cmd.version=0.1.0 -X 'github.com/HeavyHorst/remco/cmd.buildDate=$(date -u '+%Y-%m-%d %H:%M:%S')' -X github.com/HeavyHorst/remco/cmd.commit=`git rev-parse HEAD`"
+CGO_ENABLED=0 go build -a -tags netgo -ldflags "-w -X github.com/HeavyHorst/remco/cmd.version=0.1.0 -X 'github.com/HeavyHorst/remco/cmd.buildDate=$(date -u '+%Y-%m-%d %H:%M:%S')' -X github.com/HeavyHorst/remco/cmd.commit=`git rev-parse HEAD`"
