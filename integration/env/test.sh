0ea047be20d476f158d6709eaf396e20e215e0da
Rene Kaufmann <kaufmann.r@gmail.com>
2016-09-03 17:14:05 +0200

    integration/env/test.sh: fixed typo

--- integration/env/test.sh
+++ integration/env/test.sh
@@ -5,7 +5,7 @@ export APPDATA_DATABASE_PASSWORD="p@sSw0rd"
 export APPDATA_DATABASE_PORT="3306"
 export APPDATA_DATABASE_USERNAME="remco"
 export APPDATA_DATABASE_APP1="10.0.1.10:8080"
-export APPDATA_DATABASE_APP2="20.0.1.11:8080"
+export APPDATA_DATABASE_APP2="10.0.1.11:8080"
 
 remco --config integration/env/env.toml
 cmp /tmp/remco-basic-test.conf ./integration/config/test.config || cat /tmp/remco-basic-test.conf
