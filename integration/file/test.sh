9a0b549443fc6812f47204dd53930a0136b156c6
Rene Kaufmann <kaufmann.r@gmail.com>
2016-09-03 15:17:39 +0200

    fixed file integration test

--- integration/file/test.sh
+++ integration/file/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-remco config file -c integration/file/file.toml
+remco --config integration/file/file.toml
 cmp /tmp/remco-basic-test.conf ./integration/config/test.config || cat /tmp/remco-basic-test.conf
