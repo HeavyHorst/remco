f411a8761d270f8d4f51503b3dd8911fbd328b43
Rene Kaufmann <kaufmann.r@gmail.com>
2016-09-23 19:23:03 +0200

    build.sh: a little bit more pretty now

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build -a -tags netgo -ldflags "-w -X main.version=0.2.0 -X 'main.buildDate=$(date -u '+%Y-%m-%d %H:%M:%S')' -X main.commit=`git rev-parse HEAD`"
+version=0.2.1-dev
+date=`date -u '+%Y-%m-%d %H:%M:%S'`
+commit=`git rev-parse HEAD`
+
+CGO_ENABLED=0 go build -a -tags netgo -ldflags \
+		"-w -X 'main.version=$version' 
+            -X 'main.buildDate=$date' 
+			-X 'main.commit=$commit'"
