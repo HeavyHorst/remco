36646d964dc40cb2c6669c14896998706fca7e39
Rene Kaufmann <kaufmann.r@gmail.com>
2016-10-20 20:00:33 +0200

    removed the configmap support - the client is bloated

--- config.go
+++ config.go
@@ -15,7 +15,6 @@ import (
 	"sync"
 
 	"github.com/HeavyHorst/remco/backends"
-	"github.com/HeavyHorst/remco/backends/configmap"
 	"github.com/HeavyHorst/remco/backends/consul"
 	"github.com/HeavyHorst/remco/backends/env"
 	"github.com/HeavyHorst/remco/backends/etcd"
@@ -34,13 +33,12 @@ type tomlConf struct {
 	Resource  []struct {
 		Template []*template.ProcessConfig
 		Backend  struct {
-			Etcd      *etcd.Config
-			File      *file.Config
-			Env       *env.Config
-			Consul    *consul.Config
-			Vault     *vault.Config
-			Redis     *redis.Config
-			Configmap *configmap.Config
+			Etcd   *etcd.Config
+			File   *file.Config
+			Env    *env.Config
+			Consul *consul.Config
+			Vault  *vault.Config
+			Redis  *redis.Config
 		}
 	}
 }
@@ -89,13 +87,12 @@ func (c *tomlConf) run(stop chan bool) {
 	for _, v := range c.Resource {
 		var backendList []template.Backend
 		backendConfigMap := map[string]template.BackendConfig{
-			"etcd":      v.Backend.Etcd,
-			"file":      v.Backend.File,
-			"env":       v.Backend.Env,
-			"consul":    v.Backend.Consul,
-			"vault":     v.Backend.Vault,
-			"redis":     v.Backend.Redis,
-			"configmap": v.Backend.Configmap,
+			"etcd":   v.Backend.Etcd,
+			"file":   v.Backend.File,
+			"env":    v.Backend.Env,
+			"consul": v.Backend.Consul,
+			"vault":  v.Backend.Vault,
+			"redis":  v.Backend.Redis,
 		}
 
 		for name, config := range backendConfigMap {
