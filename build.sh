95ccb8b94494c897df0a405303e5d872385ff887
Rene Kaufmann <kaufmann.r@gmail.com>
2016-10-20 21:18:52 +0200

    build.sh: build for linux,darwin,windows

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-version=0.3.0
+version=0.3.1
 date=`date -u '+%Y-%m-%d %H:%M:%S'`
 commit=`git rev-parse HEAD`
+os_list=( "linux" "darwin" "windows" )
 
-CGO_ENABLED=0 go build -a -tags netgo -ldflags \
+for os in "${os_list[@]}"
+do
+    echo "Build remco for $os"
+    GOOS=${os} CGO_ENABLED=0 go build -a -tags netgo -o bin/remco_${os} -ldflags \
 		"-w -X 'main.version=$version' 
             -X 'main.buildDate=$date' 
 			-X 'main.commit=$commit'"
+
+    zip -r bin/remco_${version}_${os}_amd64.zip bin/remco_${os}
+done
