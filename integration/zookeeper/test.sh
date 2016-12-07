41b70fdb0d46cd3c4431d2b6fa46d460d7f02b07
Rene Kaufmann <kaufmann.r@gmail.com>
2016-12-07 20:22:37 +0100

    fixed the zookeeper version number in the zk integration test

--- integration/zookeeper/test.sh
+++ integration/zookeeper/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-zookeeper-3.4.8/bin/zkCli.sh create /appdata ""
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/database ""
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/upstream ""
+zookeeper-3.4.9/bin/zkCli.sh create /appdata ""
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/database ""
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/upstream ""
 
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/database/password "p@sSw0rd"
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/database/port "3306"
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/database/username "remco"
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/database/password "p@sSw0rd"
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/database/port "3306"
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/database/username "remco"
 
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/upstream/app1 "10.0.1.10:8080"
-zookeeper-3.4.8/bin/zkCli.sh create /appdata/upstream/app2 "10.0.1.11:8080"
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/upstream/app1 "10.0.1.10:8080"
+zookeeper-3.4.9/bin/zkCli.sh create /appdata/upstream/app2 "10.0.1.11:8080"
 
 remco --config integration/zookeeper/zookeeper.toml
 cat /tmp/remco-basic-test.conf
