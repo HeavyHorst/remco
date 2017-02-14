ca103fb2980b1f98be838529b928a0dab6026029
Rene Kaufmann <kaufmann.r@gmail.com>
2017-02-14 12:41:19 +0100

    log.go: don't log line numbers was always the line where the logger was defined - not useful.

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -11,8 +11,6 @@ package log
 import (
 	"fmt"
 	"os"
-	"runtime"
-	"strings"
 	"sync"
 
 	log "github.com/Sirupsen/logrus"
@@ -72,56 +70,32 @@ func SetLevel(level string) error {
 	return nil
 }
 
-// some log middleware handlers
-
-//withSource adds the field source to the logs
-//example: source=resource.go:310
-func withSource(l *log.Entry) *log.Entry {
-	if log.GetLevel() == log.DebugLevel {
-
-		lock.Lock()
-		defer lock.Unlock()
-
-		_, file, line, ok := runtime.Caller(2)
-		if !ok {
-			file = "<???>"
-			line = 1
-		} else {
-			slash := strings.LastIndex(file, "/")
-			file = file[slash+1:]
-		}
-		logger := l.WithField("source", fmt.Sprintf("%s:%d", file, line))
-		return logger
-	}
-	return l
-}
-
 // Debug logs a message with severity DEBUG.
 func Debug(v ...interface{}) {
-	withSource(logger).Debug(v)
+	logger.Debug(v)
 }
 
 // Error logs a message with severity ERROR.
 func Error(v ...interface{}) {
-	withSource(logger).Error(v)
+	logger.Error(v)
 }
 
 // Fatal logs a message with severity ERROR followed by a call to os.Exit().
 func Fatal(v ...interface{}) {
-	withSource(logger).Fatal(v)
+	logger.Fatal(v)
 }
 
 // Info logs a message with severity INFO.
 func Info(v ...interface{}) {
-	withSource(logger).Info(v)
+	logger.Info(v)
 }
 
 // Warning logs a message with severity WARNING.
 func Warning(v ...interface{}) {
-	withSource(logger).Warning(v)
+	logger.Warning(v)
 }
 
 // WithFields is a Wrapper for logrus.WithFields
 func WithFields(fields log.Fields) *log.Entry {
-	return withSource(logger).WithFields(fields)
+	return logger.WithFields(fields)
 }
