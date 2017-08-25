a4277f45e31e54dbd8874ad5b2d3e509e119f2e5
Rene Kaufmann <kaufmann.r@gmail.com>
2017-08-25 13:22:41 +0200

    pongo2: updated dependency to version v3.1

--- vendor/github.com/HeavyHorst/pongo2/pongo2_template_test.go
+++ vendor/github.com/HeavyHorst/pongo2/pongo2_template_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/flosch/pongo2"
+	"github.com/HeavyHorst/pongo2"
 )
 
 var adminList = []string{"user2"}
--- vendor/github.com/HeavyHorst/pongo2/pongo2_test.go
+++ vendor/github.com/HeavyHorst/pongo2/pongo2_test.go
@@ -3,7 +3,7 @@ package pongo2_test
 import (
 	"testing"
 
-	"github.com/flosch/pongo2"
+	"github.com/HeavyHorst/pongo2"
 	. "gopkg.in/check.v1"
 )
 
