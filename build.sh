66e7c05ab1772fff4814d9ff99f304fdad3b1ef1
Rene Kaufmann <kaufmann.r@gmail.com>
2017-10-07 14:15:06 +0200

    build.sh: fixed indentation

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ do
     echo "Build remco for $os"
     GOOS=${os} CGO_ENABLED=0 go build -a -tags netgo -o bin/remco_${os} -ldflags \
 		"-w -X 'main.version=$version' 
-            -X 'main.buildDate=$date' 
-			-X 'main.commit=$commit'" \
-            github.com/HeavyHorst/remco/cmd/remco
+		-X 'main.buildDate=$date' 
+		-X 'main.commit=$commit'" \
+		github.com/HeavyHorst/remco/cmd/remco
 
     cd bin && zip -r remco_${version}_${os}_amd64.zip remco_${os} && cd ..
 done
