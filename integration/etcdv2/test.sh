bc70c9db94aef15a6cd6f815273a10ccd0ff2f58
Rene Kaufmann <kaufmann.r@gmail.com>
2019-02-03 11:14:22 +0100

    integrations: etcd endpoints (parse 127.0.0.1:2379: first path segment in URL cannot contain colon)

--- integration/etcdv2/test.sh
+++ integration/etcdv2/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/database/host 127.0.0.1
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/database/password p@sSw0rd
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/database/port 3306
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/database/username remco
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/upstream/app1 10.0.1.10:8080
-etcdctl --endpoints=127.0.0.1:2379 set /appdata/upstream/app2 10.0.1.11:8080
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/database/host 127.0.0.1
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/database/password p@sSw0rd
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/database/port 3306
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/database/username remco
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/upstream/app1 10.0.1.10:8080
+etcdctl --endpoints=http://127.0.0.1:2379 set /appdata/upstream/app2 10.0.1.11:8080
 
 remco --config integration/etcdv2/etcd.toml
 cat /tmp/remco-basic-test-etcdv2.conf
